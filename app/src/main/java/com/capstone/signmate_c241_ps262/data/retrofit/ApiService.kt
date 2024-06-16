afbd3057ba1a971c813e299a71118739d7add9d4
furqanramadhan <furqan21@mhs.unsyiah.ac.id>
2024-06-16 17:55:12 +0700

    edit opening id

--- app/src/main/java/com/capstone/signmate_c241_ps262/data/retrofit/ApiService.kt
+++ app/src/main/java/com/capstone/signmate_c241_ps262/data/retrofit/ApiService.kt
@@ -2,4 +2,5 @@ package com.capstone.signmate_c241_ps262.data.retrofit
 
 interface ApiService {
 
+
 }
--- app/src/main/java/com/capstone/signmate_c241_ps262/ui/opening/OpeningActivity.kt
+++ app/src/main/java/com/capstone/signmate_c241_ps262/ui/opening/OpeningActivity.kt
@@ -3,15 +3,15 @@ package com.capstone.signmate_c241_ps262.ui.opening
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.capstone.signmate_c241_ps262.databinding.OpeningActivityBinding
+import com.capstone.signmate_c241_ps262.databinding.ActivityOpeningBinding
 
 
 class OpeningActivity : AppCompatActivity() {
-    private lateinit var binding: OpeningActivityBinding
+    private lateinit var binding: ActivityOpeningBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = OpeningActivityBinding.inflate(layoutInflater)
+        binding = ActivityOpeningBinding.inflate(layoutInflater)
         window.statusBarColor = Color.parseColor("#256656")
         setContentView(binding.root)
     }
