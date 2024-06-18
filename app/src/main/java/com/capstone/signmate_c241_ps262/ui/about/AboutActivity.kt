e49b25ce88a5a2cb4a296f666290c2636c028005
furqanramadhan <furqan21@mhs.unsyiah.ac.id>
2024-06-18 14:27:28 +0700

    edit id popup

--- app/src/main/java/com/capstone/signmate_c241_ps262/ui/about/AboutActivity.kt
+++ app/src/main/java/com/capstone/signmate_c241_ps262/ui/about/AboutActivity.kt
@@ -1,9 +1,11 @@
 package com.capstone.signmate_c241_ps262.ui.about
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.signmate_c241_ps262.databinding.ActivityAboutBinding
+import com.capstone.signmate_c241_ps262.ui.dashboard.DashboardActivity
 
 class AboutActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBinding
@@ -14,5 +16,9 @@ class AboutActivity : AppCompatActivity() {
         window.statusBarColor = Color.parseColor("#256656")
         setContentView(binding.root)
 
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
